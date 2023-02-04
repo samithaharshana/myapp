623c4cd1207900cdf6aa10bc56568994d218e185
Samitha Palihawadana <Samitha_08768@dialog.dialoggsm.com>
2023-02-04 22:31:43 +0530

    fifth

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! ma java world" );
+        System.out.println( "Hello World! ma java world test build" );
     }
 }
